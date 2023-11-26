f6d48364d4b8879d08b0c0ce501fe05c716c6d20
brunomry <brunomadozzo@gmail.com>
2023-11-26 20:54:27 -0300

    Resolución completa de ejercicio N°1 - TP5.

--- JS/ejercicio1-tp5.js
+++ JS/ejercicio1-tp5.js
@@ -1,24 +1,54 @@
 const btnEnviar = document.getElementById('enviar'); 
 const form = document.querySelector('form');
 
-const compararNumero = (e) => {
+const obtenerNumero = () => document.getElementById('inputNumber').value;
+
+const validarNumero = numeroIngresado => !isNaN(numeroIngresado) && (numeroIngresado >= 0 || numeroIngresado <= 10)
+
+const generarNumero = () => Math.floor(Math.random() * 11);
+
+let numeroRandom = generarNumero();
+let numeroIngresado;
+
+const esGanador = (numeroIngresado, numeroRandom, intentosUsuario) => {
+  if(numeroIngresado == numeroRandom){
+    return true;
+  }else if(numeroIngresado < numeroRandom && intentosUsuario < 3){
+    alert(`Estuviste cerca! El número ingresado es menor que el número a adivinar`);
+  }else if(intentosUsuario < 3){
+    alert(`Estuviste cerca! El número ingresado es mayor que el número a adivinar`);
+  }
+  return false;
+}
+
+let intentosUsuario = 0;
+const intentosTotales = 3;
+
+const adivinarNumero = (e) => {
   e.preventDefault();
 
-  let numero = Math.floor(Math.random() * 11);
-  const inputNumber = document.getElementById('inputNumber');
-  let numeroIngresado = inputNumber.value;
+  numeroIngresado = obtenerNumero(); 
+  intentosUsuario++;
 
-  if (isNaN(numeroIngresado)) {
-    alert(`Por favor, asegúrate de ingresar un número`);
-  }else if (numeroIngresado === numero) {
-    alert(`¡Bien por ti! Adivinaste el número. ¡Felicitaciones!`);
-  } else if (numeroIngresado < numero) {
-    alert(`El número que ingresaste es menor al número que tenías que adivinar`);
-  } else {
-    alert(`El número que ingresaste es mayor al número que tenías que adivinar`);
+  if(!validarNumero(numeroIngresado)){
+    alert(`ingresa un numero válido`);
+    form.reset();
+    return;
   }
-  
+
+  if(esGanador(numeroIngresado, numeroRandom, intentosUsuario)) {
+    alert(`Bien por ti, ganaste!! 🥳 Número Ganador: ${numeroIngresado}`);
+    numeroRandom = generarNumero();
+    intentosUsuario = 0;
+  }
+
+  if(intentosUsuario === intentosTotales){
+    alert(`Casi lo logras! Se te acabaron los intentos. Vuelve a jugar`);
+    numeroRandom = generarNumero();
+    intentosUsuario = 0;
+  }
+
   form.reset();
 }
 
-form.addEventListener('submit', compararNumero);
+form.addEventListener('submit', adivinarNumero);
